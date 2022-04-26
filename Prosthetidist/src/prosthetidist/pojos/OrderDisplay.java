3ec282c7cc9507672020bbb9e1f57c56557acb77
javih <javih@10.60.91.32>
2022-04-26 17:00:45 +0200

    Swing

---
+++ Prosthetidist/src/prosthetidist/pojos/OrderDisplay.java
@@ -0,0 +1,67 @@
+package prosthetidist.pojos;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+
+public class OrderDisplay extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OrderDisplay frame = new OrderDisplay();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OrderDisplay() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Company");
+		rdbtnNewRadioButton.setBounds(33, 52, 103, 21);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Type");
+		rdbtnNewRadioButton_1.setBounds(33, 89, 103, 21);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Price");
+		rdbtnNewRadioButton_2.setBounds(33, 132, 103, 21);
+		contentPane.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Materials");
+		rdbtnNewRadioButton_3.setBounds(33, 176, 103, 21);
+		contentPane.add(rdbtnNewRadioButton_3);
+		
+		JButton btnNewButton = new JButton("Ok");
+		btnNewButton.setBounds(258, 232, 85, 21);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cancel");
+		btnNewButton_1.setBounds(51, 232, 85, 21);
+		contentPane.add(btnNewButton_1);
+	}
+}
