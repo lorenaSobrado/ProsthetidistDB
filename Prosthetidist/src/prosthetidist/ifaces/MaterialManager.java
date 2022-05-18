074bc87cc28b43d2b23ca75c2dde945d8cf27f6e
User <User@10.60.59.107>
2022-05-19 01:59:17 +0200

    ya

---
+++ Prosthetidist/src/prosthetidist/ifaces/MaterialManager.java
@@ -0,0 +1,13 @@
+package prosthetidist.ifaces;
+
+import java.util.ArrayList;
+
+import prosthetidist.pojos.Material;
+import prosthetidist.pojos.Prosthetic;
+
+public interface MaterialManager {
+	public Material getMaterialByName(String name);
+	public ArrayList<Material> getMaterialsFromProstheticCode(Integer code);
+	public void uploadMaterialsOfProsthetic(Material material, Prosthetic prosthetic);
+
+}
---
+++ Prosthetidist/src/prosthetidist/jdbc/JDBCMaterialManager.java
@@ -0,0 +1,80 @@
+package prosthetidist.jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import prosthetidist.ifaces.MaterialManager;
+import prosthetidist.pojos.Material;
+import prosthetidist.pojos.Prosthetic;
+
+public class JDBCMaterialManager implements MaterialManager {
+
+	private JDBCManager manager;
+
+	public JDBCMaterialManager(JDBCManager m) {
+		this.manager = m;
+	}
+
+	public Material getMaterialByName(String name) {
+		Material mat = null;
+		try {
+			String sql = "SELECT * FROM Material WHERE name = ?";
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			prep.setString(1, name);
+			ResultSet rs = prep.executeQuery();
+
+			Float price = rs.getFloat("price");
+			String strength = rs.getString("strength");
+			String flexibility = rs.getString("flexibility");
+			String temperatureResistence = rs.getString("temperatureResistence");
+
+			mat = new Material(name, price, strength, flexibility, temperatureResistence);
+
+			rs.close();
+			prep.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return mat;
+	}
+
+	public ArrayList<Material> getMaterialsFromProstheticCode(Integer code) {
+		ArrayList<Material> materials = new ArrayList<Material>();
+
+		try {
+			Statement stat = manager.getConnection().createStatement();
+			String sql2 = "SELECT material_name FROM ProstheticHasMaterials WHERE prosthetic_code = " + code;
+			ResultSet rs = stat.executeQuery(sql2);
+			while (rs.next()) {
+				String name = rs.getString("material_name");
+				Material mat = this.getMaterialByName(name);
+				materials.add(mat);
+			}
+			rs.close();
+			stat.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return materials;
+	}
+
+	public void uploadMaterialsOfProsthetic(Material material, Prosthetic prosthetic) {
+		try {
+			String sql = "INSERT INTO ProstheticHasMaterials (prosthetic_code, material_name) VALUES (?,?)";
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			prep.setInt(1, prosthetic.getCode());
+			prep.setString(2, material.getName());
+
+			prep.executeUpdate();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+
+}
